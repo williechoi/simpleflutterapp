f3dd092e98b94298fedb30de2fa32c209a9dac43
Hyungsuk Choi <choihyungsuk@gmail.com>
2022-07-01 08:51:25 +0900

    add const to main page route

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:table_calendar/table_calendar.dart';
 import 'package:simpleapp/firstpage.dart';
 import 'package:simpleapp/secondpage.dart';
 
@@ -18,8 +17,8 @@ class MyApp extends StatelessWidget {
       title: "Named routes Demo",
       initialRoute: '/',
       routes: {
-        '/': (context) => FirstPage(),
-        '/second': (context) => SecondPage(),
+        '/': (context) => const FirstPage(),
+        '/second': (context) => const SecondPage(),
       },
     );
   }
